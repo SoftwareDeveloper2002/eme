4e70b822bfb1385cd2e08a4ac396435cf302e9e3
Robby Roda <robbyroda.00@gmail.com>
2026-03-01 13:18:05 +0800

    asd

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./cloud.db"
+DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -15,4 +17,4 @@ def get_db():
     try:
         yield db
     finally:
-        db.close()  
+        db.close()
